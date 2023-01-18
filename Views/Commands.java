2576c40196cf9a4b54cd09632ed7e3b5bd6f39a3
Aleksey Valentinov <ds83351@chebnet.com>
2023-01-18 18:48:40 +0300

    Add Views

---
+++ Views/Commands.java
@@ -0,0 +1,11 @@
+package Views;
+
+public enum Commands {
+    NONE,
+    READ,
+    CREATE,
+    UPDATE,
+    LIST,
+    DELETE,
+    EXIT
+}
---
+++ Views/ViewUser.java
@@ -0,0 +1,48 @@
+package Views;
+
+import Controllers.UserController;
+import Model.User;
+
+import java.util.Scanner;
+
+public class ViewUser {
+
+    private UserController userController;
+
+    public ViewUser(UserController userController) {
+        this.userController = userController;
+    }
+
+    public void run(){
+        Commands com = Commands.NONE;
+
+        while (true) {
+            String command = prompt("Введите команду READ or CREATE(UPDATE, LIST or DELETE). Для выхода введите EXIT : ");
+            com = Commands.valueOf(command);
+            if (com == Commands.EXIT) return;
+            switch (com) {
+                case CREATE:
+                    String firstName = prompt("Имя: ");
+                    String lastName = prompt("Фамилия: ");
+                    String phone = prompt("Номер телефона: ");
+                    userController.saveUser(new User(firstName, lastName, phone));
+                    break;
+                case READ:
+                    String id = prompt("Идентификатор пользователя: ");
+                    try {
+                        User user = userController.readUser(id);
+                        System.out.println(user);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+            }
+        }
+    }
+
+    private String prompt(String message) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        return in.nextLine();
+    }
+}
